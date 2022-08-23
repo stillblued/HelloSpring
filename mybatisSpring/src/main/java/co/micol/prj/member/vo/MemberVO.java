ac21b6913b31f8872f45acdaafd2169d5cd79cc9
yun <primula374@naver.com>
2022-08-23 15:44:17 +0900

    220823

---
+++ mybatisSpring/src/main/java/co/micol/prj/member/vo/MemberVO.java
@@ -0,0 +1,16 @@
+package co.micol.prj.member.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberVO {
+
+	public String member_id;
+	public String member_password;
+	public String member_name;
+	public String member_author;
+	
+	
+}
